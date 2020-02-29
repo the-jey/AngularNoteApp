445ace5cbd9a06d6b6707eba9855bae8500f6d55
the-jey <jey.pley@gmail.com>
2020-02-29 10:38:37 +0100

    add the filter functionality

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
@@ -85,15 +85,101 @@ export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
 
+  filteredNotes: Note[] = new Array<Note>();
+
+  @ViewChild('filterInput', {static: false}) filterInputElRef: ElementRef<HTMLInputElement>;
+
   constructor(private notesService: NotesService) { }
 
   ngOnInit() {
     //We want to retrieve all notes from NoteService
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notesService.getAll();
   }
 
-  deleteNote(id: number) {
-    this.notesService.delete(id);
+  deleteNote(note: Note) {
+    let noteId = this.notesService.getId(note);
+    this.notesService.delete(noteId);
+    this.filter(this.filterInputElRef.nativeElement.value);
+  }
+
+  generateNoteURL(note: Note) {
+    let noteId = this.notesService.getId(note);
+    return noteId;
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+
+    let allResults: Note[] = new Array<Note>();
+    // split uo the search query into inviduals words
+    let terms: string[] = query.split(' '); // split on spaces
+    // remove duplicate search terms
+    terms = this.removeDuplicates(terms);
+    //compile all relevant results into the AllResults array
+    terms.forEach(term => {
+      let results = this.relevantNotes(term);
+      //append results to the allResults array
+      allResults = [...allResults, ...results];
+    });
+
+    //allResults will include duplicate notes
+    //because a particular note can be the result of many search terms
+    // but we don't show that on the UI
+    // so we first must remove duplicates
+    let uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
+
+    //now sort by revelancy
+    this.sortByRevelancy(allResults);
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+    // loop the array
+    arr.forEach(e => uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  }
+
+  relevantNotes(query: string) :Array<Note>{
+    query = query.toLowerCase().trim();
+    let relevantNotes  = this.notes.filter(note => {
+      if (note.title && note.title.toLowerCase().includes(query)) {
+        return true;
+      }if (note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }
+      return false;
+    })
+
+    return relevantNotes;
+
+  }
+
+  sortByRevelancy(searchResults: Note[]) {
+    let noteCountObj: Object = {};
+
+    searchResults.forEach(note => {
+      let noteId = this.notesService.getId(note);
+
+      if(noteCountObj[noteId]) {
+        noteCountObj[noteId] += 1;
+      } else {
+        noteCountObj[noteId] = 1;
+      }
+    });
+
+    this.filteredNotes = this.filteredNotes.sort((a: Note, b: Note) => {
+      let aId = this.notesService.getId(a);
+      let bId = this.notesService.getId(b);
+
+      let aCount = noteCountObj[aId];
+      let bCount = noteCountObj[bId];
+
+      return bCount - aCount;
+    })
+
   }
 
 }
